353a4beabc20339a7fc87b9863aa79ac7e531701
Mark Galpin <markg@jfrog.com>
2016-05-21 16:00:18 -0700

    Adding libs-dev repositories for gradle build

--- configArt/configArt.sh
+++ configArt/configArt.sh
@@ -1,6 +1,7 @@
 curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.RemoteRepositoryConfiguration+json" -X PUT "$DOCKER_ART_URL/api/repositories/swampup" -T swampup.json
-curl -u admin:password -X POST -H 'Content-Type: application/json' "$DOCKER_ART_URL/api/plugins/execute/addLayout" -T tomcat-layout.json
 curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.RemoteRepositoryConfiguration+json" -X PUT "$DOCKER_ART_URL/api/repositories/apache-archive" -T apache-archive.json
 curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.LocalRepositoryConfiguration+json" -X PUT "$DOCKER_ART_URL/api/repositories/tomcat-local" -T tomcat-local.json
+curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.LocalRepositoryConfiguration+json" -X PUT "$DOCKER_ART_URL/api/repositories/libs-dev-local" -T libs-dev-local.json
+curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.VirtualRepositoryConfiguration+json" -X PUT "$DOCKER_ART_URL/api/repositories/libs-dev" -T libs-dev.json
 
 ./warmup.sh
