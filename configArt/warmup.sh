79906171242a9fbb10423fc653335529a3849026
Mark Galpin <markg@jfrog.com>
2016-05-21 14:38:49 -0700

    Removed use of tomcat-layout and switched to simple-default

--- configArt/warmup.sh
+++ configArt/warmup.sh
@@ -1,1 +1,2 @@
-curl -I "$DOCKER_ART_URL/apache-archive-cache/tomcat/tomcat-8/v8.0.32/bin/apache-tomcat-8.0.32.tar.gz"
+curl -I "$DOCKER_ART_URL/apache-archive/tomcat/tomcat-8/v8.0.32/bin/apache-tomcat-8.0.32.tar.gz"
+curl -I "$DOCKER_ART_URL/apache-archive/tomcat/tomcat-8/v8.0.35/bin/apache-tomcat-8.0.35.tar.gz"
