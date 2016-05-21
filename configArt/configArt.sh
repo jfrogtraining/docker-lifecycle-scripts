4fc8bb760d5710bfa19f24a274e3f5793149fa8a
Mark Galpin <markg@jfrog.com>
2016-05-21 16:10:17 -0700

    Updated jcenter to point local

--- configArt/configArt.sh
+++ configArt/configArt.sh
@@ -1,5 +1,6 @@
 curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.RemoteRepositoryConfiguration+json" -X PUT "$DOCKER_ART_URL/api/repositories/swampup" -T swampup.json
 curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.RemoteRepositoryConfiguration+json" -X PUT "$DOCKER_ART_URL/api/repositories/apache-archive" -T apache-archive.json
+curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.RemoteRepositoryConfiguration+json" -X POST "$DOCKER_ART_URL/api/repositories/jcenter" -T jcenter.json
 curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.LocalRepositoryConfiguration+json" -X PUT "$DOCKER_ART_URL/api/repositories/tomcat-local" -T tomcat-local.json
 curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.LocalRepositoryConfiguration+json" -X PUT "$DOCKER_ART_URL/api/repositories/libs-dev-local" -T libs-dev-local.json
 curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.VirtualRepositoryConfiguration+json" -X PUT "$DOCKER_ART_URL/api/repositories/libs-dev" -T libs-dev.json
