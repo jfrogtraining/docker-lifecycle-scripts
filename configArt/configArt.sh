76f37201535510dba1bf1cd53dec9178ca58db00
Mark Galpin <markg@jfrog.com>
2016-05-21 14:13:19 -0700

    Moved EXPORT of docker_art_url into command for broader usage

--- configArt/configArt.sh
+++ configArt/configArt.sh
@@ -1,7 +1,6 @@
-export DOCKER_ART_URL="http://192.168.243.128:8081/artifactory"
-
 curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.RemoteRepositoryConfiguration+json" -X PUT "$DOCKER_ART_URL/api/repositories/swampup" -T swampup.json
 curl -u admin:password -X POST -H 'Content-Type: application/json' "$DOCKER_ART_URL/api/plugins/execute/addLayout" -T tomcat-layout.json
 curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.RemoteRepositoryConfiguration+json" -X PUT "$DOCKER_ART_URL/api/repositories/apache-archive" -T apache-archive.json
 curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.LocalRepositoryConfiguration+json" -X PUT "$DOCKER_ART_URL/api/repositories/tomcat-local" -T tomcat-local.json
 
+./warmup.sh
---
+++ configArt/warmup.sh
@@ -0,0 +1,1 @@
+curl -I "$DOCKER_ART_URL/apache-archive-cache/tomcat/tomcat-8/v8.0.32/bin/apache-tomcat-8.0.32.tar.gz"
