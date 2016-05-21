de14c10fbad523e6eae2964dfddeca9eb08163f4
Mark Galpin <markg@jfrog.com>
2016-05-21 14:47:20 -0700

    Updated bootstrap to reflect latestVersion search as per generic

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,6 @@ cd docker-framework/jdk
 jfrog rt dl --flat=true swampup/java/jdk-8u91-linux-x64.tar.gz
 mv jdk-8u* jdk-8-linux-x64.tar.gz
 cd ../tomcat
-jfrog rt dl --flat=true swampup/tomcat/apache-tomcat-8.0.35.tar.gz
-mv apache-tomcat-8.0* apache-tomcat-8.tar.gz
+export TOMCAT_VERSION=$(curl -uadmin:password "$DOCKER_ART_URL/api/search/latestVersion?g=org.apache&a=apache-tomcat&repos=tomcat-local")
+jfrog rt dl --flat=true "tomcat-local/org/apache/apache-tomcat/apache-tomcat-$TOMCAT_VERSION.tar.gz"
+mv "apache-tomcat-$TOMCAT_VERSION.tar.gz" apache-tomcat-8.tar.gz
