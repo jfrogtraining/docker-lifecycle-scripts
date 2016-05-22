6b11479a96b8b451ed78deb27b52660805ab1ec4
Mark Galpin <markg@jfrog.com>
2016-05-21 21:02:42 -0700

    Trying to conform to environment

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 cd docker-framework/jdk
-jfrog rt dl --flat=true swampup/java/jdk-8u91-linux-x64.tar.gz
+jfrog rt dl jswampup-remote/java/jdk-8u91-linux-x64.tar.gz
 mv jdk-8u* jdk-8-linux-x64.tar.gz
 cd ../tomcat
 export TOMCAT_VERSION=$(curl -uadmin:password "$DOCKER_ART_URL/api/search/latestVersion?g=org.apache&a=apache-tomcat&repos=tomcat-local")
-jfrog rt dl --flat=true "tomcat-local/org/apache/apache-tomcat/apache-tomcat-$TOMCAT_VERSION.tar.gz"
+jfrog rt dl "tomcat-local/org/apache/apache-tomcat/apache-tomcat-$TOMCAT_VERSION.tar.gz"
 mv "apache-tomcat-$TOMCAT_VERSION.tar.gz" apache-tomcat-8.tar.gz
