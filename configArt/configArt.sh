e17518e74ba6dc59b0c9a9993554468180b283ee
Mark Galpin <markg@jfrog.com>
2016-05-23 11:56:11 -0700

    Adding docker-dev, docker-prod virtual repos

--- configArt/configArt.sh
+++ configArt/configArt.sh
@@ -5,5 +5,8 @@ curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.re
 curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.LocalRepositoryConfiguration+json" -X PUT "$DOCKER_ART_URL/api/repositories/tomcat-local" -T tomcat-local.json
 curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.LocalRepositoryConfiguration+json" -X PUT "$DOCKER_ART_URL/api/repositories/libs-dev-local" -T libs-dev-local.json
 curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.VirtualRepositoryConfiguration+json" -X PUT "$DOCKER_ART_URL/api/repositories/libs-dev" -T libs-dev.json
+curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.VirtualRepositoryConfiguration+json" -X PUT "$DOCKER_ART_URL/api/repositories/docker-prod" -T docker-prod.json
+curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.VirtualRepositoryConfiguration+json" -X PUT "$DOCKER_ART_URL/api/repositories/docker-dev" -T docker-dev.json
+curl -uadmin:password -X DELETE "$DOCKER_ART_URL/api/repositories/docker-virtual"
 
 ./warmup.sh
