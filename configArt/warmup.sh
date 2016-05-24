ce2922375733a5140b6179c69748dab91b5f3086
Mark Galpin <markg@jfrog.com>
2016-05-23 23:06:10 -0700

    Updating to use art.local and warm-up java

--- configArt/warmup.sh
+++ configArt/warmup.sh
@@ -1,2 +1,3 @@
 curl -I "$DOCKER_ART_URL/apache-archive/tomcat/tomcat-8/v8.0.32/bin/apache-tomcat-8.0.32.tar.gz"
 curl -I "$DOCKER_ART_URL/apache-archive/tomcat/tomcat-8/v8.0.35/bin/apache-tomcat-8.0.35.tar.gz"
+curl -I "$DOCKER_ART_URL/swampup/java/jdk-8u91-linux-x64.tar.gz"
